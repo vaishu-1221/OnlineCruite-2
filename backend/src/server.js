b98ad218bb1c88412cfaa8ec5200c7165c18ee55
vaishu-1221 <vaishavirajput028@gmail.com>
2026-01-20 11:38:35 +0530

    auth db sync added

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,11 +2,18 @@ import express from 'express'
 import {ENV} from './lib/env.js';
 import path from 'path';
 import { connectDB } from './lib/db.js';
+import cors from 'cors';
+import {functions, inngest} from './lib/inngest.js';
 
 const app =express();
 
 const __dirname = path.resolve();
 
+//middleware
+app.use(express.json());
+app.use(cors({origin:ENV.CLIENT_URL,credentials:true})); //credential true means cookies can be sent
+
+app.use('/api/inngest',serve({client:inngest,functions}))
 
 app.get('/health',(req,res)=>{
     res.status(200).json({msg:'success from api'});
@@ -14,6 +21,7 @@ app.get('/health',(req,res)=>{
 app.get('/boom',(req,res)=>{
     res.status(200).json({msg:'success from api boom'});
 })
+
 
 if(ENV.NODE_ENV==='production'){
     app.use(express.static(path.join(__dirname,'../frontend/dist')))
