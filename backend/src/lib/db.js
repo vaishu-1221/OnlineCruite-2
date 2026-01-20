65cd0fa7b2ccb96a54cd6c93ed9467b97b11c2e7
vaishu-1221 <vaishavirajput028@gmail.com>
2026-01-20 09:48:40 +0530

    validation check added MONGO_URL

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -3,6 +3,9 @@ import {ENV} from "./env.js";
 
 export const connectDB=async()=>{
     try{
+        if(!ENV.MONGODB_URL){
+            throw new Error("MONGODB_URL is not defined in environment variables");
+        }
         const conn=await mongoose.connect(ENV.MONGODB_URL);
         console.log("Mongoose Connected successfully",conn.connection.host);
     }catch(err){
