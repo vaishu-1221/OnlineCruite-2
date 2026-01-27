deb95c70a3624c3506b26d4b9bcb4900836da228
vaishu-1221 <vaishavirajput028@gmail.com>
2026-01-27 19:47:46 +0530

    session page is also added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,33 @@
-import { Navigate, Route, Routes } from 'react-router-dom';
-import HomePage from './pages/HomePage';
 import { useUser } from "@clerk/clerk-react";
+import { Navigate, Route, Routes } from "react-router";
+import HomePage from "./pages/HomePage";
 
-import ProblemPage from './pages/ProblemPage';
-import { Toaster } from 'react-hot-toast';
-import DashboardPage from './pages/DashboardPage';
-import ProblemDetailPage from './pages/ProblemDetailPage';
+import { Toaster } from "react-hot-toast";
+import DashboardPage from "./pages/DashboardPage";
+import ProblemPage from "./pages/ProblemDetailPage";
+import ProblemsPage from "./pages/ProblemPage";
+import SessionPage from "./pages/SessionPage";
 
 function App() {
-  const {isSignedIn,isLoaded}=useUser();
+  const { isSignedIn, isLoaded } = useUser();
 
-  if(!isLoaded) return null;
+  // this will get rid of the flickering effect
+  if (!isLoaded) return null;
+
   return (
     <>
-    <Routes>
-        
+      <Routes>
+        <Route path="/" element={!isSignedIn ? <HomePage /> : <Navigate to={"/dashboard"} />} />
+        <Route path="/dashboard" element={isSignedIn ? <DashboardPage /> : <Navigate to={"/"} />} />
 
-        <Route path='/' element={!isSignedIn ? <HomePage/> : <Navigate to={"/dashboard"} />}/>
-        <Route path='/dashboard' element={isSignedIn ? <DashboardPage/>:<Navigate to={"/"} /> }/>
-        <Route path='/problems' element={isSignedIn ? <ProblemPage/>:<Navigate to={"/"} /> }/>
-        <Route path='/problem/:id' element={isSignedIn ? <ProblemDetailPage/> : <Navigate to={"/"}/>}/>
-  
+        <Route path="/problems" element={isSignedIn ? <ProblemsPage /> : <Navigate to={"/"} />} />
+        <Route path="/problem/:id" element={isSignedIn ? <ProblemPage /> : <Navigate to={"/"} />} />
+        <Route path="/session/:id" element={isSignedIn ? <SessionPage /> : <Navigate to={"/"} />} />
+      </Routes>
 
-    </Routes>
-    <Toaster toastOptions={{duration:3000}}/>
+      <Toaster toastOptions={{ duration: 3000 }} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
